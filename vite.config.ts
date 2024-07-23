c0dd84dacaf6242a94714d753475fbc950211e07
Daniel Wyatt <daniel.ryuichi.wyatt@gmail.com>
2024-07-23 09:59:34 -0500

    fixed HMR

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import eslint from "@nabla/vite-plugin-eslint";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint(), viteTsconfigPaths()],
   server: {
     open: true,
   },
